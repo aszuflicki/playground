701813f5bbc34285250c4a841734d5ab1115b105
Arkadiusz <a.szuflicki@gmail.com>
2019-04-11 14:01:05 +0200

    Screenshot

---
+++ screenshot/index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://facebook.com');
+const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
